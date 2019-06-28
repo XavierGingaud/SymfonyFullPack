81b066160dd5d4d2d128ef6e35a5def51c37ee20
skip.desneiges21@gmail.com <xa.vier2170@gmail.com>
2019-06-28 16:40:15 +0200

    Basic files

---
+++ exo2/src/Form/BienType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bien;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BienType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('image')
+            ->add('category')
+            ->add('owner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Bien::class,
+        ]);
+    }
+}
